b6d0e57fe662127777b0b7a31b40da7e072d9746
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-28 08:17:23 +0200

    installing packs and making map

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,38 @@
+/* eslint import/no-webpack-loader-syntax: off */
+
+import { Map, NavigationControl, Marker, GeolocateControl } from "react-map-gl";
+import mapboxgl from "mapbox-gl";
 
 
-const Home = () => {
+
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+
+
+const MyMap = () => {
     return ( 
-        <section>
-            <h1>This is home page</h1>
-        </section>
+        <Map 
+            mapLib={import('mapbox-gl')}
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+            style={{width: "85vw", height: "50vh"}}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            initialViewState={{
+               latitude: 55.7060378,
+               longitude: 12.5142235,
+               zoom: 16
+            }}
+        >
+         <NavigationControl />  
+
+         <Marker latitude={55.7060378} longitude={12.5142235} color="hotpink" />
+        <GeolocateControl 
+            trackUserLocation={true}
+            positionOptions={{ enableHighAcuracy: true }}
+            showAcuracyCircle={true}
+            auto
+        />
+
+        </Map>
      );
 }
  
-export default Home;
+export default MyMap;
